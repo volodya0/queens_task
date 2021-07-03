4a67e6b2ce814ca8dc458b71eb6e22caf74b22db
unknown <volodauzuk02@gmail.com>
2021-07-03 17:47:30 +0300

    v3

--- src/main.ts
+++ src/main.ts
@@ -1,89 +1,86 @@
-const size : number = 8
-const count : number = 8
-
+let size : number = 8
 let solutions : number[][] = []
 
 const isNewSolution = (positions : number[]) : boolean => {
-  return (solutions === []) ? true : solutions.every(sol => {
-    const set = new Set(sol)
-    positions.forEach(e => set.add(e))
-    return set.size !== sol.length
-  })
+  return (solutions === []) ? true : solutions.every(sol => JSON.stringify(sol) !== JSON.stringify(positions))
 }
 
 const isFreeField = (y : number, x : number, positions : number[]) : boolean => {
-  return positions.every(num => {
-    const Py = Math.floor(num / size)
-    const Px = num % size
-    return Py !== y && Px !== x && Math.abs(Py - y) !== Math.abs(Px - x)
-  })
+  return positions.every((Px, Py) =>  Py !== y && Px !== x && Math.abs(Py - y) !== Math.abs(Px - x))  
 }
 
-
 const func = (positions: number[] = []) : void => {
-  for(let row : number = 0; row < size; row++){
-    if(positions.every(num => Math.floor(num / size) !== row))
+  for(let row : number = positions.length; row < size; row++){
       for(let colum : number = 0; colum < size; colum++){
 
         if(isFreeField(row, colum, positions)){   
-
-          const positionsBackup = [...positions]
-
-          positions.push(row*size+colum)
+          
+          positions.push(colum)
 
           if(positions.length === size && isNewSolution(positions)){
-            solutions.push(positions)
+            solutions.push([...positions])
           }
 
           func(positions)
 
-          if(positions[0] === size){
-            return
-          }
-
-          positions = positionsBackup
+          positions.pop()
 
         }
+
+        if(colum === size - 1) return
+
       }
   }
 }
 
+let start : HTMLElement = document.getElementById("start-btn")
+let result : HTMLElement = document.getElementById("result")
 
-
-
-
-let start = document.getElementById("start-btn")
-let result = document.getElementById("result")
-
-let desk = document.getElementById("desk")
-let list = document.getElementById("list")
+let desk : HTMLElement = document.getElementById("desk")
+let list : HTMLElement = document.getElementById("list")
+let input : HTMLInputElement | HTMLElement = document.getElementById("input")
+list.style.display = 'none'
 
 start.addEventListener('click', () => {
 
-  const startTime = performance.now()
+  solutions = []
+  desk.innerHTML=''
+  list.innerHTML=''
+  list.style.display = 'none'
+  result.style.display = 'none'
 
+  if('value' in input && +input.value > 0 ) size = + input.value
+  else return
+  
+  const startTime = performance.now()
   func()
   
-  const time = performance.now() - startTime
-  
-  result.innerText = 'Result : ' + solutions.length + ' Time : ' + (time)
+  result.style.display = 'block'
+  result.innerText = 'Result : ' + solutions.length + ' Time : ' + (performance.now() - startTime)
 
   solutions.forEach((sol, i) => {
     const element = document.createElement('p')
-    element.innerText = i + 1 + '. ' + sol.reduce((acc, num) => acc += `[${Math.floor(num/size)}, ${num%size}] `, '')
+    element.innerText = i + 1 + '. ' + sol.reduce((acc, colum, row) => acc += `[${row}, ${colum}] `, '')
     element.addEventListener('click', render.bind(null, sol))
     list.appendChild(element)
   })
 
-  render(solutions[0])
+  if(solutions.length){
+    render(solutions[0])
+    list.style.display = 'block'
+  }
+  else{
+    list.style.display = 'none'
+  }
 
 })
 
 const render = (positions : number[]) : void => {
 
   desk.innerHTML=''
+  
   const area = new Array(size).fill(null).map(() => new Array(size).fill(false))
-  positions.forEach(num => area[Math.floor(num/size)][num%size] = true);
+  positions.forEach((colum, row) => area[row][colum] = true);
 
   area.forEach((row, rowIndex) => {
     const r = document.createElement('div')
